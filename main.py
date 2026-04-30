095058d8032a7c6cc492d436359c9f19c28812e2
projektked <projektked@gmail.com>
2026-04-30 12:49:32 +0200

    sentiment analisis

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import matplotlib.pyplot as plt
 import nltk
+import pandas as pd
+from nltk.sentiment import SentimentIntensityAnalyzer
+import datetime
 
-test_setence="test for github repo3"
-print(test_setence)
-print(test_setence)
-print(test_setence)
-print(test_setence)
+nltk.download('vader_lexicon')
+sentimentAnalyzer = SentimentIntensityAnalyzer()
+
+df = pd.read_csv('')
+df = df.drop(columns=['author', 'over_18', 'time_created', 'subreddit'])
+df['date_created'] = df['date_created'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").date())
+
+df['sentiment'] = df['title'].apply(lambda x: sentimentAnalyzer.polarity_scores(x)['compound'])
+
+df = df.groupby(df.date_created.map(lambda x: x.day)).mean()
+
+plt.plot(df['date_created'], df['sentiment'])
+
+plt.xlabel('sentiment')
+plt.ylabel('date')
+
+plt.show()
