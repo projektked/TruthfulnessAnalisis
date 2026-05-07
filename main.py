923c42de8ccc915eb5c44126ab319efd8a891755
projektked <projektked@gmail.com>
2026-05-07 12:55:00 +0200

    wordmap

--- main.py
+++ main.py
@@ -2,22 +2,42 @@ import matplotlib.pyplot as plt
 import nltk
 import pandas as pd
 from nltk.sentiment import SentimentIntensityAnalyzer
-import datetime
+import re
+from wordcloud import STOPWORDS
+from wordcloud import WordCloud
 
 nltk.download('vader_lexicon')
 sentimentAnalyzer = SentimentIntensityAnalyzer()
 
 df = pd.read_csv('')
 df = df.drop(columns=['author', 'over_18', 'time_created', 'subreddit'])
-df['date_created'] = df['date_created'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").date())
 
 df['sentiment'] = df['title'].apply(lambda x: sentimentAnalyzer.polarity_scores(x)['compound'])
 
-df = df.groupby(df.date_created.map(lambda x: x.day)).mean()
+text = ' '.join(df['title'].astype(str).tolist())
+text = re.sub(r'[^A-Za-z\s]', '', text)
+text = text.lower()
 
-plt.plot(df['date_created'], df['sentiment'])
+stopwords = set('s').union(STOPWORDS)
+text = ' '.join(word for word in text.split() if word not in stopwords)
 
-plt.xlabel('sentiment')
-plt.ylabel('date')
-
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.title("Wordcloud of data")
 plt.show()
+
+text_arr = text.split()
+text_arr_filtered = list(set(text_arr))
+word_polartity_dict = dict()
+
+for x in text_arr:
+    word_polartity_dict[x]=sentimentAnalyzer.polarity_scores(x)['compound']
+
+
+
+print(word_polartity_dict)
+# if df['sentiment'].mean()>0:
+#
+# else if df['sentiment'].mean()<0:
